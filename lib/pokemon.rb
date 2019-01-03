7fe0bdc916d37e43a97bd653361958c817aa96d8
Sean Langton <slangton1@gmail.com>
2019-01-03 21:44:11 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ class Pokemon
 
   def self.find(number, db)
     ins = db.prepare('SELECT name, type FROM pokemon WHERE id = ?')
-    ins2 = db.prepare('SELECT name, type FROM pokemon')
+    ins2 = db.prepare('SELECT * FROM pokemon where id = ?')
     binding.pry
     pokemon = ins.execute(number)
 #    id:id, name:name, type:type, db:db
