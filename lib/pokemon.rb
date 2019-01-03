91148a28eb5caf86dc2a8d506eae9d196e14c2ea
Sean Langton <slangton1@gmail.com>
2019-01-03 21:37:20 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,11 +10,11 @@ class Pokemon
     ins.execute(name, type)
   end
 
-  def self.find(id, db)
+  def self.find(number, db)
     name_prepare = db.prepare('select name from pokemon where id = (?)')
     type_prepare = db.prepare('select type from pokemon where id = (?)')
-    @name = name_prepare.execute(id)
-    @type = type_prepare.execute(id)
+    @name = name_prepare.execute(number)
+    @type = type_prepare.execute(number)
 #    id:id, name:name, type:type, db:db
   end
 
