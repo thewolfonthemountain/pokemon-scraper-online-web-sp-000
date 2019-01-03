662f75986b3d554f5dbd2d07ea66d16a26c5baa0
Sean Langton <slangton1@gmail.com>
2019-01-03 22:20:55 +0000

    Automatically backed up by Learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -39,7 +39,7 @@ describe "Pokemon" do
       expect(pikachu_from_db.type).to eq("electric")
     end
   end
-
+=begin
   describe "BONUS" do
 
     before do
@@ -72,3 +72,4 @@ describe "Pokemon" do
     end
   end
 end
+=end
