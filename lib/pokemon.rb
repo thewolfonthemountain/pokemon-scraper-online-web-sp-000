83dda894854d06bd8f8a9c4550055eacc79ef9ef
Sean Langton <slangton1@gmail.com>
2019-01-03 22:03:57 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,8 +15,5 @@ class Pokemon
 
   def self.find(number, db)
     pokemon = db.execute('SELECT * FROM pokemon WHERE id = (?)', [number])
-    @id = pokemon[0]
-    @name = pokemon[1]
-    @type = pokemon[2]
   end
 end
